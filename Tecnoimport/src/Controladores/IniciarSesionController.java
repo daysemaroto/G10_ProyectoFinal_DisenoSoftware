09d43d695f84659c8d71c770564095745922b755
Luis Carrasco <cluis0152@gmail.com>
2019-08-19 17:05:36 -0500

    mejora login

--- Tecnoimport/src/Controladores/IniciarSesionController.java
+++ Tecnoimport/src/Controladores/IniciarSesionController.java
@@ -21,6 +21,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 /**
@@ -31,6 +32,8 @@ import javafx.scene.control.TextField;
 public class IniciarSesionController implements Initializable {
     @FXML
     private TextField txtUser,txtPass;
+    @FXML
+    private Label lbldbstatus;
     
     static Connection conn = ConexionADB.getConexion_DB();
 
@@ -39,52 +42,67 @@ public class IniciarSesionController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        if (conn!=null) {lbldbstatus.setText(lbldbstatus.getText()+" Conectada");lbldbstatus.setStyle("-fx-text-fill: green;");
+        }else{lbldbstatus.setText(lbldbstatus.getText()+" Offline");lbldbstatus.setStyle("-fx-text-fill: red;");}
     }  
     
     @FXML
     private void iniciarSesion(){
-        try {
-            
-            String query = "select * from login where idUsuario =? and clave =?";
-            PreparedStatement ps = conn.prepareStatement(query);
-            ps.setInt(1, Integer.parseInt(txtUser.getText()));
-            ps.setString(2, txtPass.getText());
-            
-            ResultSet  rs = ps.executeQuery();
-            
-            //probar 909090909 xxx123
-            
-            //cambiar de ventana aqui
-            if(rs.next())
-            {
-                    switch(rs.getString(3)){
-                        case "Gerente":
-                            System.out.println("Gerente");
-                            break;
-                        case "Administrador":
-                            System.out.println("Administrador");
-                            break;
-                        case "Vendedor":
-                            System.out.println("Vendedor");
-                            break;
-                        case "Jefe de Bodega":
-                            System.out.println("Jefe de Bodega");
-                            break;
-                    }
-            }
-            else{
-                Alert alert = new Alert(AlertType.ERROR);
-                alert.setTitle("Error Dialog");
-                alert.setHeaderText("Look, an Error Dialog");
-                alert.setContentText("Ooops, there was an error!");
+        if(!txtPass.getText().isEmpty() && !txtUser.getText().isEmpty()){
+            try {
 
-                alert.showAndWait();  
+                String query = "select * from login where idUsuario =? and clave =?";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setInt(1, Integer.parseInt(txtUser.getText()));
+                ps.setString(2, txtPass.getText());
+
+                ResultSet  rs = ps.executeQuery();
+
+                //probar 909090909 xxx123
+
+                //cambiar de ventana aqui
+                if(rs.next())
+                {
+                        switch(rs.getString(3)){
+                            case "Gerente":
+                                System.out.println("Gerente");
+                                break;
+                            case "Administrador":
+                                System.out.println("Administrador");
+                                break;
+                            case "Vendedor":
+                                System.out.println("Vendedor");
+                                break;
+                            case "Jefe de Bodega":
+                                System.out.println("Jefe de Bodega");
+                                break;
+                        }
                 }
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(IniciarSesionController.class.getName()).log(Level.SEVERE, null, ex);
+                else{
+                    Alert alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Usuario y/ocontraseña no validos !");
+                    alert.setContentText("Ooops, no se encontro el usuario!");
+
+                    alert.showAndWait();  
+                    }
+
+            } catch (SQLException ex) {
+                Logger.getLogger(IniciarSesionController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }else{
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Informacion");
+            alert.setHeaderText("Verifique usuario y/o contraseña");
+            alert.setContentText("Ooops, usuario y contraseña necesarios!");
+
+            alert.showAndWait(); 
         }
+    }
+    
+    @FXML
+    private void cancelar(){
+        System.exit(0);
     }
     
 }
